91868e3319d8ee03aba586ccbd7d7894d2883d9f
federico <federico@federico-MOV>
2017-04-17 19:37:19 -0300

    cree paquete

--- TheBestGameInTheWorld/src/Modelo/Conversacion.java
+++ TheBestGameInTheWorld/src/Modelo/Conversacion.java
@@ -3,14 +3,11 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package thebestgameintheworld;
+package Modelo;
 
 /**
  *
  * @author Juani
  */
 public class Conversacion {
-    
-    
-//p546155654645645645
 }
